dfe8a9267308060680fd940adfdd6165f5b4a8c4
priya <priya@host.docker.internal>
2021-05-18 11:36:26 +0530

    changes

--- src/main/java/com/cg/lms/entity/Address.java
+++ src/main/java/com/cg/lms/entity/Address.java
@@ -8,7 +8,16 @@ import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class Address {
 	
 	@Id
@@ -23,7 +32,6 @@ public class Address {
 	@JoinColumn(name = "userId")
 	private Users users;
 
-	public Address() {}
 
 	public Address(int addressId, String address1, String address2, String city, String state, int pincode) {
 		super();
@@ -33,69 +41,5 @@ public class Address {
 		this.city = city;
 		this.state = state;
 		this.pincode = pincode;
-	}
-
-	public int getAddressId() {
-		return addressId;
-	}
-
-	public void setAddressId(int addressId) {
-		this.addressId = addressId;
-	}
-
-	public String getAddress1() {
-		return address1;
-	}
-
-	public void setAddress1(String address1) {
-		this.address1 = address1;
-	}
-
-	public String getAddress2() {
-		return address2;
-	}
-
-	public void setAddress2(String address2) {
-		this.address2 = address2;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public int getPincode() {
-		return pincode;
-	}
-
-	public void setPincode(int pincode) {
-		this.pincode = pincode;
-	}
-
-	@JsonBackReference
-	public Users getUsers() {
-		return users;
-	}
-
-	public void setUsers(Users users) {
-		this.users = users;
-	}
-
-	// ToString
-	@Override
-	public String toString() {
-		return "Address [addressId=" + addressId + ", address1=" + address1 + ", address2=" + address2 + ", city="
-				+ city + ", state=" + state + ", pincode=" + pincode +"]";
 	}
 }
