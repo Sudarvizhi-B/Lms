a281208ee07ba9433b339d8f827989229b68c955
Sandhyana-N <82045623+Sandhyana-N@users.noreply.github.com>
2021-06-07 10:21:03 +0530

    reader and feedback

--- src/main/java/com/cg/lms/controller/ReaderController.java
+++ src/main/java/com/cg/lms/controller/ReaderController.java
@@ -2,9 +2,11 @@ package com.cg.lms.controller;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +19,10 @@ import com.cg.lms.entity.Reader;
 import com.cg.lms.exception.ReaderNotFoundException;
 import com.cg.lms.service.IReaderService;
 
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 public class ReaderController {
+	org.apache.logging.log4j.Logger logger= LogManager.getLogger(ReaderController.class);
 	
 	@Autowired
 	IReaderService readerService;
@@ -27,6 +31,7 @@ public class ReaderController {
 	// Register reader
 	@PostMapping("/reader")
 	public ResponseEntity<Reader> registerReader(@RequestBody Reader reader) {
+		logger.info("Register reader");
 		Reader registeredReader=readerService.register(reader);
 		return new ResponseEntity<>(registeredReader, HttpStatus.OK);
 	}
@@ -34,6 +39,7 @@ public class ReaderController {
 	// Update reader details
 	@PutMapping("/reader/{id}")
 	public ResponseEntity<Reader> updateReaderdetail(@PathVariable("id") int id, @RequestBody Reader reader) {
+		logger.info("Update reader details");
 		if (readerService.updateReaderDetails(reader) == null) {
 			throw new ReaderNotFoundException(EXCEPTION + id);
 		}
@@ -44,13 +50,15 @@ public class ReaderController {
 	// View all the readers
 	@GetMapping("/reader")
 	public ResponseEntity<List<Reader>> viewReaderList(Reader reader) {
+		logger.info("view all readers");
 		List<Reader> readerList=readerService.viewReadersList();
 		return new ResponseEntity<>(readerList, HttpStatus.OK);
 	}
 	
 	// View reader by id
-	@GetMapping("/reader/{id}")
+	@GetMapping("/reader/get/{id}")
 	public ResponseEntity<Reader> viewReaderById(@PathVariable("id") int id) {
+		logger.info("view reader by Id");
 		if (readerService.viewReaderById(id) == null) {
 			throw new ReaderNotFoundException(EXCEPTION + id);
 		}
@@ -61,6 +69,7 @@ public class ReaderController {
 	// Delete reader by id
 	@DeleteMapping("/reader/{id}")
 	public ResponseEntity<Reader> deleteReader(@PathVariable("id") int id) {
+		logger.info("delete reader by Id");
 		if (readerService.deleteReader(id) == null) {
 			throw new ReaderNotFoundException(EXCEPTION + id);
 		}
